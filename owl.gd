e4e184c8f0a8564a535fe620f921698b142323a0
ltong4867 <114775582+ltong4867@users.noreply.github.com>
2025-09-20 20:05:30 +0000

    Change graph axis and make owl follow the graph

--- owl.gd
+++ owl.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends PathFollow2D
 
 # Speed at which the image moves
 var move_speed : int = 200
@@ -8,20 +8,5 @@ func _ready():
 	pass
 
 func _process(delta):
-	# Get input direction
-	var move_direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("space"):
-		move_direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		move_direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		move_direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		move_direction.y -= 1
-	
-	# Normalize direction to avoid faster movement diagonally
-	move_direction = move_direction.normalized()
-
-	# Move the sprite
-	position += move_direction * move_speed * delta
+	# The movement is now handled by the main script
+	pass
