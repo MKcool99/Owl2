7b698c3ae9983b8847d4dd52f4d537b3305e56b1
ltong4867 <114775582+ltong4867@users.noreply.github.com>
2025-09-21 00:32:02 +0000

    untested v3

--- owl.gd
+++ owl.gd
@@ -1,27 +1,5 @@
-extends Sprite2D
-
-# Speed at which the image moves
-var move_speed : int = 200
+extends PathFollow2D
 
 func _ready():
-	# Initial setup if necessary
+	# The owl's movement is handled by the main script.
 	pass
-
-func _process(delta):
-	# Get input direction
-	var move_direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("ui_right"):
-		move_direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		move_direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		move_direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		move_direction.y -= 1
-	
-	# Normalize direction to avoid faster movement diagonally
-	move_direction = move_direction.normalized()
-
-	# Move the sprite
-	position += move_direction * move_speed * delta
